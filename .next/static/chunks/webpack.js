7d6e409415682343f0fe10758f4cfcd23f67c552
Thierry Jones <thierry@proxi.id>
2023-10-31 18:01:02 -0400

    schema fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e7e3121853e8c29d"; }
+/******/ 		__webpack_require__.h = function() { return "9407e27d567a2860"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import Main from '../components/Main';
 import Skills from '../components/Skills';
 
 export default function Home() {
-  const jsonLdData = `
-  {
+  const jsonLdData = {
     "@context": "http://schema.org",
     "@type": "Organization",
     "name": "Thierry Jones",
@@ -18,17 +17,22 @@ export default function Home() {
     "founder": {
       "@type": "Person",
       "name": "Thierry Jones"
-    }
-  }
+    },
+    "logo": "https://www.thierryjones.ca/public/Logo.png" 
   
-  `;
+  };
 
   return (
     <div>
       <Head>
         <title>Thierry Jones</title>
-        <link rel="icon" href="/Logo.png" />
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: jsonLdData }} />
+        <link rel="icon" href="./public/Logo.png" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(jsonLdData),
+          }}
+        />
       </Head>
       <Main />
       <About />
