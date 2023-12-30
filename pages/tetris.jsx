b0f26e2fb5ca788d22c057b4ebdaab8707f26527
TJones <jones.thierry@gmail.com>
2023-12-30 18:25:43 -0500

    minor tetris

--- pages/tetris.jsx
+++ pages/tetris.jsx
@@ -5,7 +5,6 @@ const TetrisPage = () => {
     <div className="min-h-screen flex justify-center items-center">
       <div className="max-w-screen-lg w-full p-2 text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to Tetris</h1>
-        <h2 className="text-2xl font-bold mb-4">It is a work in progress dont sue me</h2>
         <Tetris />
       </div>
     </div>
